22897fcdb262f2a8ea28bed280f2f86018d5d5bf
mercurytc75 <mercurytc74@gmail.com>
2025-11-28 13:03:35 -0600

    modified

--- ListaDinamica.java
+++ ListaDinamica.java
@@ -22,4 +22,40 @@ public class ListaDinamica<E> implements Lista<E> {
         }
         tamanio++;
     }
+    @Override
+    public void addLast(E elemento){
+        Nodo<E> nuevo = new Nodo<>(elemento);
+        if(isEmpty()){
+            cabeza = cola = nuevo;
+        }else{
+            cola.siguiente = nuevo,
+            cola = nuevo;
+        }
+        tamanio++;
+    }
+    @Override
+    public void addEnesima(int indice, E elemento){
+        if(indice < 0 || indice > tamanio){
+            throw new IndexOutOfBoundsException("indice fuera de rango");
+        }
+        if(indice == 0){
+            addFirst(elemento);
+        }else if(indice == tamanio){
+            addLast(elemento);
+        }else{
+            Nodo<E> actual = cabeza;
+            for(int i = 0; i < indice - 1; i++){
+                actual = actual.siguiente;
+            }
+            Nodo<E> nuevo = new Nodo<>(elemento);
+            nuevo.siguiente = actual.siguiente;
+            actual.siguiente = nuevo;
+            tamanio++;
+
+        }
+    }
+    @Override
+    public E removeFirst(){
+        
+    }
 }
